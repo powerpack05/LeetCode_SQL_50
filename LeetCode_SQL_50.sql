ea4ecd1e7ce98af3f13a84513c0502b3816f3db5
Farheensyed05 <sdfarheen05@gmail.com>
2024-07-31 21:34:38 +0530

    Leet Code Question 11

--- LeetCode_SQL_50.sql
+++ LeetCode_SQL_50.sql
@@ -264,3 +264,42 @@ SELECT machine_id,ROUND(AVG(time_difference),3) as processing_time FROM cte
 GROUP BY machine_id;
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------
 
+-- Q11.) Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+
+-- Create Employee table
+CREATE TABLE Employee (
+    empId INT PRIMARY KEY,
+    name VARCHAR(100),
+    supervisor INT,
+    salary INT
+);
+
+-- Insert data into Employee table
+INSERT INTO Employee (empId, name, supervisor, salary)
+VALUES
+(3, 'Brad', NULL, 4000),
+(1, 'John', 3, 1000),
+(2, 'Dan', 3, 2000),
+(4, 'Thomas', 3, 4000);
+
+-- Create Bonus table
+CREATE TABLE Bonus (
+    empId INT PRIMARY KEY,
+    bonus INT,
+    FOREIGN KEY (empId) REFERENCES Employee(empId)
+);
+
+-- Insert data into Bonus table
+INSERT INTO Bonus (empId, bonus)
+VALUES
+(2, 500),
+(4, 2000);
+
+
+SELECT e.name,b.bonus FROM employee e
+LEFT JOIN bonus b ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL
+ORDER BY e.name;
+
+-- -----------------------------------------------------------------------------------------------------------------------------
+
