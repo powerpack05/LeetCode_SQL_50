a7e17e1eb0e478d277b3dd686d51880f1c9e7002
Farheensyed05 <sdfarheen05@gmail.com>
2024-08-27 00:12:37 +0530

    updated new question

--- LeetCode_SQL_50.sql
+++ LeetCode_SQL_50.sql
@@ -302,7 +302,8 @@ WHERE b.bonus < 1000 OR b.bonus IS NULL
 ORDER BY e.name;
 
 -- -----------------------------------------------------------------------------------------------------------------------------
--- Q12.)Write a solution to find the number of times each student attended each exam.Return the result table ordered by student_id and subject_name.
+-- Q12.)Write a solution to find the number of times each student attended each exam.Return the result table ordered 
+-- by student_id and subject_name.
 
 -- Create Students table
 CREATE TABLE Students (
@@ -406,7 +407,50 @@ WHERE id IN (
     HAVING COUNT(*) >= 5
 );
 
--- -----------------------------------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------------------------------------------------------------------------
+-- Q14.) Write a solution to find the confirmation rate of each user.Return the result table in any order.
+-- The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages.
+-- The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
+
+CREATE TABLE Signups (
+    user_id INT PRIMARY KEY,
+    time_stamp DATETIME
+);
+
+INSERT INTO Signups (user_id, time_stamp) VALUES (3, '2020-03-21 10:16:13');
+INSERT INTO Signups (user_id, time_stamp) VALUES (7, '2020-01-04 13:57:59');
+INSERT INTO Signups (user_id, time_stamp) VALUES (2, '2020-07-29 23:09:44');
+INSERT INTO Signups (user_id, time_stamp) VALUES (6, '2020-12-09 10:39:37');
+
+CREATE TABLE Confirmations (
+    user_id INT,
+    time_stamp DATETIME,
+    action ENUM('confirmed', 'timeout'),
+    PRIMARY KEY (user_id, time_stamp),
+    FOREIGN KEY (user_id) REFERENCES Signups(user_id)
+);
+
+INSERT INTO Confirmations (user_id, time_stamp, action) VALUES (3, '2021-01-06 03:30:46', 'timeout');
+INSERT INTO Confirmations (user_id, time_stamp, action) VALUES (3, '2021-07-14 14:00:00', 'timeout');
+INSERT INTO Confirmations (user_id, time_stamp, action) VALUES (7, '2021-06-12 11:57:29', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action) VALUES (7, '2021-06-13 12:58:28', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action) VALUES (7, '2021-06-14 13:59:27', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action) VALUES (2, '2021-01-22 00:00:00', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action) VALUES (2, '2021-02-28 23:59:59', 'timeout');
+
+
+SELECT s.user_id,ROUND(AVG(CASE
+						WHEN c.action = 'confirmed' THEN 1.0
+                        ELSE 0.00
+					END),2) as confirmation_rate
+	FROM signups s
+LEFT JOIN confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
+
+-- ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 
 
 
